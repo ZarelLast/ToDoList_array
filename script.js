673ed8769c4b0b12cad00365e377c5fd20437238
Zarel Last <lastzarel@gmail.com>
2019-01-04 23:58:21 +0700

    finish

---
+++ script.js
@@ -0,0 +1,62 @@
+let todos = [];
+const listDOM = document.getElementById('list');
+const inputDOM = document.getElementById('input-todo');
+const addDOM = document.getElementById('add-btn');
+
+const data = JSON.parse(localStorage.getItem('todos'));
+console.log(data);
+todos = data;
+
+function render(){
+	let index = 0;
+	listDOM.innerHTML = "";
+	while(index<todos.length){
+		console.log(index);
+		listDOM.innerHTML += '<li><div class="posted"><input id="box" type="checkbox" />&nbsp<label class="strike">'+ todos[index]+'</label></div><button class="btn-del" onclick=hapus('+index+')><span>&times</span></button></li>';
+		index = index + 1;
+	}
+}
+
+addDOM.addEventListener('click',function(){
+	const todo = inputDOM.value;
+	todos.push(todo);
+	console.log(todos);
+	render();
+	localStorage.setItem('todos',JSON.stringify(todos));
+})
+
+function hapus(index){
+	console.log(index); 
+	todos.splice(index, 1)
+	render();
+	localStorage.setItem('todos',JSON.stringify(todos));
+}
+const nomorDOM = document.getElementById('nomor');
+nomorDOM.innerHTML += todos.length+' To Do';
+
+function showTime(){
+	var date = new Date();
+	var h = date.getHours();
+	var m = date.getMinutes();
+	
+	var session =" AM";
+	if(h==0){
+		h=12;
+	}
+	if(h>12){
+		h=h-12;
+		session = " PM";
+	}
+	h = (h<10)? "0" + h :h;
+	m = (m<10)? "0" + m :m;
+	var time = h + ":" + m;
+	var pm = session;
+	document.getElementById("s").innerText = pm;
+	document.getElementById("s").textContent = pm;
+	document.getElementById("MyClockDisplay").innerText = time;
+	document.getElementById("MyClockDisplay").textContent = time;
+	setTimeout(showTime, 1000);
+	}	
+
+showTime();
+render();
