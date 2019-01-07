399494b87e8a904e25bd99e6cc60becf418b2141
Zarel Last <lastzarel@gmail.com>
2019-01-07 08:30:40 +0700

    add data

--- java.js
+++ java.js
@@ -7,8 +7,6 @@ const hardisk = window.localStorage;
 const data = JSON.parse(hardisk.getItem('todos'));
 console.log(data);
 
-todos = data;
-
 function render(){
 	let index = 0;
 	listDOM.innerHTML = "";
@@ -17,6 +15,7 @@ function render(){
 		listDOM.innerHTML += "<li><div class='posted'><input id='box' type='checkbox' />&nbsp<label class='strike'>"+todos[index]+"</label></div><button class='btn-del' onclick=hapus("+index+")><span>&times</span></button></li>";
 		index = index + 1;
 	}
+	todos = data;
 }
 
 addDOM.addEventListener('click',function(){
