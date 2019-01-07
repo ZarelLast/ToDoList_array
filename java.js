fa528f11fb57f06c36bc9ab5bf96b9a93e39be0a
Zarel Last <lastzarel@gmail.com>
2019-01-07 09:44:18 +0700

    add data

--- java.js
+++ java.js
@@ -14,10 +14,9 @@ function render(){
 		console.log(index);
 		listDOM.innerHTML += "<li><div class='posted'><input id='box' type='checkbox' />&nbsp<label class='strike'>"+todos[index]+"</label></div><button class='btn-del' onclick=hapus("+index+")><span>&times</span></button></li>";
 		index = index + 1;
-		todos = data;
 	}
 };
-
+todos = data;
 addDOM.addEventListener('click',function(){
 	const todo = inputDOM.value;
 	todos.push(todo);
